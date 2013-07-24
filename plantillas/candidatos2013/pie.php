c719e3b83a701e4f9c25fde06459e3b7b3756044
Adrian Barabino <adrian.barabino@gmail.com>
2013-07-24 00:21:12 -0300

    Arreglado un link del vendor, y el orden

--- plantillas/candidatos2013/pie.php
+++ plantillas/candidatos2013/pie.php
@@ -4,10 +4,10 @@
 		<span>Sitio realizado por Adrian Barabino</span>
 	</footer>
 	<!-- Comienzan los Vendors -->
-	<script src="./plantillas/<?php echo $plantilla; ?>/js/vendor/prefix-free.min.js"></script>
+	<script src="./plantillas/<?php echo $plantilla; ?>/js/vendor/prefixfree.min.js"></script>
 	<script src="./plantillas/<?php echo $plantilla; ?>/js/vendor/jquery.min.js"></script>
-	<script src="./plantillas/<?php echo $plantilla; ?>/js/vendor/backbone.min.js"></script>
 	<script src="./plantillas/<?php echo $plantilla; ?>/js/vendor/underscore.min.js"></script>
+	<script src="./plantillas/<?php echo $plantilla; ?>/js/vendor/backbone.min.js"></script>
 	<script src="./plantillas/<?php echo $plantilla; ?>/js/vendor/jquery.hoverIntent.min.js"></script>
 	
 	<!-- Ahora el JS propio -->
