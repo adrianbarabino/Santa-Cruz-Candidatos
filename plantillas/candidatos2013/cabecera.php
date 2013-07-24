1d95a34fafb97f2df39b568a85fa11094d69f0b6
Adrian Barabino <adrian.barabino@gmail.com>
2013-07-24 00:38:23 -0300

    Agregado estilo y contenido a inicio

--- plantillas/candidatos2013/cabecera.php
+++ plantillas/candidatos2013/cabecera.php
@@ -2,6 +2,11 @@
 <html lang="es">
 <head>
 	<meta charset="UTF-8">
+	<!-- CSS externos primero, para que se muestren bien las fuentes y los íconos -->
+	<link href='http://fonts.googleapis.com/css?family=Droid+Sans:400,700|Open+Sans:400,700|Nunito:700' rel='stylesheet' type='text/css'>
+	<link href="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/css/bootstrap-combined.no-icons.min.css" rel="stylesheet">
+	<link href="//netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.css" rel="stylesheet">
+	<!-- Ahora cargamos nuestro CSS -->
 	<link rel="stylesheet" href="./plantillas/<?php echo $plantilla; ?>/css/estilos.css">
 	<title>Santa Cruz Candidatos</title>
 </head>
